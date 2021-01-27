f3725024e3ca5a2fc93ac19aba3ee7c5f73d79b8
徐侃锋 <xu_kanfeng@192.168.0.105>
2021-01-27 23:28:01 +0800

    增加battles集合记录类型声明

--- src/pages/Battles.tsx
+++ src/pages/Battles.tsx
@@ -2,13 +2,36 @@ import React from 'react'
 import Wechat from '../services/wechat'
 import { Button } from 'antd'
 
+interface BattlesDoc {
+  createTime: string;
+  isFinished: boolean;
+  isOverdue: boolean;
+  isStarted: boolean;
+  lastUpdate: string;
+  managerOpenId: string;
+  players: [];
+  playersCountToRecord: number;
+  records: [];
+  reserveFees: boolean;
+  totalReserveFees: number;
+  verifySum: boolean;
+  wxaCodeUrl: string;
+  _id: string;
+  _openid: string;
+}
+
 const Battles: React.FC<any> = props => {
   const onRequest = () => {
     const date = '2021-01-27'
     Wechat.requestRpc(
       `db.collection('battles').where({
         createTime: _.gte(new Date('${date}'))
-      }).get()`).then(data => console.log(data))
+      }).get()`)
+      .then((data: any) => data.data)
+      .then((docs: BattlesDoc[]) => {
+        const res = docs.filter((doc: BattlesDoc) => doc.records.length > 0)
+        console.log(res)
+      })
   }
   return <div>
     <Button type="primary" onClick={onRequest}>查询</Button>
